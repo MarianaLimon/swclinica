956805f751762b7e1c4d85c53019a844f67daebf
MarianaLimon <marianalimong@hotmail.com>
2016-05-18 10:16:12 -0500

    se agregan nuevas interfaces

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -9,19 +9,46 @@ function config($stateProvider, $urlRouterProvider) {
             templateUrl: "views/common/content.html"
         })
 
-        .state('index.main', {
-            url: "/interdoc",
-            templateUrl: "../views/NuevoPaciente.html",
-            data: { pageTitle: 'Nuevo paciente', specialClass: 'landing-page'}
-
-
-        })
-
         .state('landing', {
             url: "/inicio",
             templateUrl: "../views/GestorClinico.html",
             data: { pageTitle: 'Login', specialClass: 'landing-page' }
+        })
 
+        .state('index.nuevo', {
+            url: "/interdoc",
+            templateUrl: "../views/NuevoPaciente.html",
+            data: { pageTitle: 'Nuevo paciente', specialClass: 'landing-page'}
+        })
+
+        .state('index.search', {
+            url: "/buscarpaciente",
+            templateUrl: "../views/BuscarPaciente.html",
+            data: { pageTitle: 'Buscar paciente', specialClass: 'landing-page'}
+        })
+
+        .state('index.registrarpagos', {
+            url: "/registrarpagos",
+            templateUrl: "../views/RegistrarPagos.html",
+            data: { pageTitle: 'Registrar Pagos', specialClass: 'landing-page'}
+        })
+
+        .state('index.historialclinico', {
+            url: "/historialclinico",
+            templateUrl: "../views/HistorialClinico.html",
+            data: { pageTitle: 'Historial Clinico', specialClass: 'landing-page'}
+        })
+
+        .state('index.historialpagos', {
+            url: "/historialpagos",
+            templateUrl: "../views/HistorialPagos.html",
+            data: { pageTitle: 'Historial de Pagos', specialClass: 'landing-page'}
+        })
+
+        .state('index.agenda', {
+            url: "/agenda",
+            templateUrl: "../views/Agenda.html",
+            data: { pageTitle: 'Agenda de Citas', specialClass: 'landing-page'}
         })
 }
 angular
